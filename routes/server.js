9dad1669820e33e977915a3ff9b8a2d9d19775a1
roychen <roychad.cy7@gmail.com>
2015-10-08 15:06:15 +0800

    .

--- routes/server.js
+++ routes/server.js
@@ -1,4 +1,20 @@
 var subapp = module.exports = require('express')();
+var uuid = require('node-uuid');
+var moment = require('moment');
+
+// use mem to store some config
+var resources = {};
+var authTokens = {};
+
+var users = [{
+  username: 'roy',
+  password: '888888',
+  age: 25
+}, {
+  username: 'roychen',
+  password: '111111',
+  age: 25
+}];
 
 subapp.get('/', function(req, res) {
   if (!req.session.username) {
@@ -19,23 +35,48 @@ subapp.get('/auth', function(req, res) {
     return res.redirect('/login');
   }
 
+  // set resources
+  var authToken = uuid.v4();
+  var accessToken = uuid.v4();
+
+  authTokens[authToken] = {
+    accessToken: accessToken,
+    createTime: moment()
+  };
+
+  resources[accessToken] = {
+    username: req.session.username,
+    age: req.session.age,
+    refreshToken: uuid.v1() 
+  }
+
   res.render('server/auth', {
-    retUrl: req.query.redirect_uri + '?state=' + req.query.state + '&code=' + 'grant_code'
+    retUrl: req.query.redirect_uri + '?state=' + req.query.state + '&code=' + authToken
   })
 });
 
 subapp.get('/auth_code', function(req, res) {
   // return resource code and refresh code by grant_code
-  if (req.query.code !== 'grant_code') {
+  if (!authTokens[req.query.code]) {
     return res.end('error code'); 
   }
-  
+
+  var now = moment();
+  var authTokenInfo = authTokens[req.query.code];
+
+  if (now - authTokenInfo.createTime > 60 * 60 * 1000) {
+    delete authTokens[req.query.code];
+    return res.end('expired code');
+  }
+
+  var resourceInfo = resources[authTokenInfo.accessToken];
+   
   res.json({
-    access_token: 'access_token',
+    access_token: authTokenInfo.assessToken,
     token_type: 'test',
-    refresh_token: 'refresh_token'
+    refresh_token: resourceInfo.refreshToken
   });
-})
+});
 
 subapp.get('/resources', function(req, res) {
   // Check code and return resource
@@ -49,15 +90,36 @@ subapp.get('/resources', function(req, res) {
 
 subapp.post('/login', function(req, res) {
   // Check login
-  if (req.body.username === 'roy' && req.body.password === '888888') {
-    req.session.username = req.body.username;
-    req.session.age = 25;
+  var userInfo = checkUser(req.body.username, req.body.password);
+  if (userInfo) {
+    // set session
+    req.session.username = userInfo.username;
+    req.session.age = userInfo.age;
+
     res.redirect('/');
   } else {
     res.redirect('/login');
   }
+  
+  function checkUser(username, password) {
+    for (var i in users) {
+      if (users[i].username === username 
+      && users[i].password === password) {
+        return user[i];
+      }
+
+      return false;
+    }
+  }
 });
 
-subapp.post('/auth', function(req, res) {
-  // Redirect to from page with code
-})
+// expired
+setInterval(function() {
+  var now = moment();
+
+  for (var i in authTokens) {
+    if (now - authTokens[i].createTime > 60 * 60 * 1000) {
+      delete authTokens[i];
+    }
+  }
+}, 3 * 1000);
